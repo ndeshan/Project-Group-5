58ee276326db4badc73abd62e6f817faa042b397
binoy969 <binoymahima1234@gmail.com>
2025-11-01 23:22:17 +0530

    final23.22

--- php/binoy/create_contact_table.php
+++ php/binoy/create_contact_table.php
@@ -3,13 +3,12 @@
 include('binoy_conn.php');
 
 
-$sql = "CREATE TABLE IF NOT EXISTS contact_us (
+$sql = "CREATE TABLE contact_us (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   phone VARCHAR(20),
-  comment TEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  comment TEXT
 )";
 
 
