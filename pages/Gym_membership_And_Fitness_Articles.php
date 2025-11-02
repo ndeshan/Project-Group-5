0877e53d081da8067fb18a2743f73e85c0d1e38f
tharupama <tharupama826@gmail.com>
2025-11-02 08:47:32 +0530

    member ship location fixed

--- pages/Gym_membership_And_Fitness_Articles.php
+++ pages/Gym_membership_And_Fitness_Articles.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['username'])){
-    header('Location: ../../php/naveen/login.html');
+    header('Location: ../php/naveen/login.html');
     exit();
 
 }
